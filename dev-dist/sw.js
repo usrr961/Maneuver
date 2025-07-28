f794dc2ea43beb08e1f02e2e1ee2277541578186
Shinyships <xxandynguyenxx@gmail.com>
2025-07-28 15:45:34 -0400

    new font

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-0a62edbb'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.atf9357pit"
+    "revision": "0.i4rqms9fduo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
